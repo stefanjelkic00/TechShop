4b29e9ad525a285fa177ba8b4c8a0ff464da11c2
Jelka00 <stefanjelkic@gmail.com>
2025-02-26 16:00:11 +0100

    Initial commit - Added backend project

---
+++ techshop/src/test/java/com/techshop/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
